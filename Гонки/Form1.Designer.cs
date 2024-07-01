b2bf9df0e521b55607f22b58e6843ec5faf07122
pc <pc@PC->
2024-07-01 21:23:49 +0300

    Добавьте файлы проекта.

---
+++ Гонки/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace Гонки
+{
+    partial class FormRace
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormRace));
+            this.pictureBoxRoad = new System.Windows.Forms.PictureBox();
+            this.Car = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxRoad2 = new System.Windows.Forms.PictureBox();
+            this.EnemyCar1 = new System.Windows.Forms.PictureBox();
+            this.EnemyCar2 = new System.Windows.Forms.PictureBox();
+            this.labelLose = new System.Windows.Forms.Label();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.Coin = new System.Windows.Forms.PictureBox();
+            this.labelCoins = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRoad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRoad2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyCar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyCar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxRoad
+            // 
+            this.pictureBoxRoad.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxRoad.Image")));
+            this.pictureBoxRoad.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxRoad.Name = "pictureBoxRoad";
+            this.pictureBoxRoad.Size = new System.Drawing.Size(840, 662);
+            this.pictureBoxRoad.TabIndex = 0;
+            this.pictureBoxRoad.TabStop = false;
+            // 
+            // Car
+            // 
+            this.Car.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Car.Image = ((System.Drawing.Image)(resources.GetObject("Car.Image")));
+            this.Car.Location = new System.Drawing.Point(433, 521);
+            this.Car.Name = "Car";
+            this.Car.Size = new System.Drawing.Size(112, 128);
+            this.Car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Car.TabIndex = 1;
+            this.Car.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // pictureBoxRoad2
+            // 
+            this.pictureBoxRoad2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxRoad2.Image")));
+            this.pictureBoxRoad2.Location = new System.Drawing.Point(0, -650);
+            this.pictureBoxRoad2.Name = "pictureBoxRoad2";
+            this.pictureBoxRoad2.Size = new System.Drawing.Size(840, 650);
+            this.pictureBoxRoad2.TabIndex = 2;
+            this.pictureBoxRoad2.TabStop = false;
+            // 
+            // EnemyCar1
+            // 
+            this.EnemyCar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.EnemyCar1.Image = ((System.Drawing.Image)(resources.GetObject("EnemyCar1.Image")));
+            this.EnemyCar1.Location = new System.Drawing.Point(289, -130);
+            this.EnemyCar1.Name = "EnemyCar1";
+            this.EnemyCar1.Size = new System.Drawing.Size(128, 128);
+            this.EnemyCar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.EnemyCar1.TabIndex = 3;
+            this.EnemyCar1.TabStop = false;
+            // 
+            // EnemyCar2
+            // 
+            this.EnemyCar2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.EnemyCar2.Image = ((System.Drawing.Image)(resources.GetObject("EnemyCar2.Image")));
+            this.EnemyCar2.Location = new System.Drawing.Point(548, -400);
+            this.EnemyCar2.Name = "EnemyCar2";
+            this.EnemyCar2.Size = new System.Drawing.Size(128, 128);
+            this.EnemyCar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.EnemyCar2.TabIndex = 4;
+            this.EnemyCar2.TabStop = false;
+            // 
+            // labelLose
+            // 
+            this.labelLose.AutoSize = true;
+            this.labelLose.BackColor = System.Drawing.Color.Red;
+            this.labelLose.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLose.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelLose.Location = new System.Drawing.Point(301, 197);
+            this.labelLose.Name = "labelLose";
+            this.labelLose.Size = new System.Drawing.Size(244, 37);
+            this.labelLose.TabIndex = 5;
+            this.labelLose.Text = "Вы проиграли !";
+            this.labelLose.Click += new System.EventHandler(this.labelLose_Click);
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.BackColor = System.Drawing.Color.Red;
+            this.buttonRestart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRestart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonRestart.Location = new System.Drawing.Point(323, 254);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(209, 33);
+            this.buttonRestart.TabIndex = 6;
+            this.buttonRestart.Text = "Начать заново";
+            this.buttonRestart.UseVisualStyleBackColor = false;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // Coin
+            // 
+            this.Coin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Coin.Image = ((System.Drawing.Image)(resources.GetObject("Coin.Image")));
+            this.Coin.Location = new System.Drawing.Point(377, -600);
+            this.Coin.Name = "Coin";
+            this.Coin.Size = new System.Drawing.Size(32, 32);
+            this.Coin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Coin.TabIndex = 8;
+            this.Coin.TabStop = false;
+            // 
+            // labelCoins
+            // 
+            this.labelCoins.AutoSize = true;
+            this.labelCoins.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.labelCoins.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCoins.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelCoins.Location = new System.Drawing.Point(12, 21);
+            this.labelCoins.Name = "labelCoins";
+            this.labelCoins.Size = new System.Drawing.Size(124, 25);
+            this.labelCoins.TabIndex = 9;
+            this.labelCoins.Text = "Монеты: 0";
+            // 
+            // FormRace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(840, 650);
+            this.Controls.Add(this.labelCoins);
+            this.Controls.Add(this.Coin);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.labelLose);
+            this.Controls.Add(this.EnemyCar2);
+            this.Controls.Add(this.EnemyCar1);
+            this.Controls.Add(this.Car);
+            this.Controls.Add(this.pictureBoxRoad);
+            this.Controls.Add(this.pictureBoxRoad2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormRace";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormRace_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRoad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRoad2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyCar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyCar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Coin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxRoad;
+        private System.Windows.Forms.PictureBox Car;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureBoxRoad2;
+        private System.Windows.Forms.PictureBox EnemyCar1;
+        private System.Windows.Forms.PictureBox EnemyCar2;
+        private System.Windows.Forms.Label labelLose;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.PictureBox Coin;
+        private System.Windows.Forms.Label labelCoins;
+    }
+}
+
---
+++ Гонки/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Гонки
+{
+    public partial class FormRace : System.Windows.Forms.Form
+    {
+        private Point pos;  // где мы нажали мышкой первый раз
+        private bool dragging;  // значения о том передвигаем ли окно или нет
+
+        private bool lose = false; // движение маишнки после проигрыша
+
+        private int countCoints = 0;
+
+        public FormRace()
+        {
+            InitializeComponent();
+
+            pictureBoxRoad.MouseDown += MouseClickDown;
+            pictureBoxRoad.MouseUp += MouseClickUp;
+            pictureBoxRoad.MouseMove += MouseClickMove;
+            // передвижение pictureBox по клику мышки 
+
+            pictureBoxRoad2.MouseDown += MouseClickDown;
+            pictureBoxRoad2.MouseUp += MouseClickUp;
+            pictureBoxRoad2.MouseMove += MouseClickMove;
+
+            // Пропадание button и label
+            labelLose.Visible = false;
+            buttonRestart.Visible = false;
+
+            // при перезапуске все работает
+            KeyPreview = true;
+        }
+
+        private void MouseClickDown(object sender, MouseEventArgs e) // нажатие мышки
+        {
+            dragging = true;
+            pos.X = e.X;
+            pos.Y = e.Y;
+        }
+
+        private void MouseClickUp(object sender, MouseEventArgs e) // отпустили мышку
+        {
+            dragging = false;
+        }
+
+        private void MouseClickMove(object sender, MouseEventArgs e) // нажали на мышку и двигаем
+        {
+            if (dragging)            
+            {
+                Point Currentpoint = PointToScreen(new Point(e.X, e.Y));  // получаем координаты где сейчас находится мышка и преобразуем к координатам экрана с помощью PointToScreen
+                this.Location = new Point(Currentpoint.X-pos.X, Currentpoint.Y-pos.Y + pictureBoxRoad.Top);  // от текущего места мышки отнимаем начальную точку мышки 
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // движение дороги с помощью двух PictureBox
+            int speed = 7;
+            pictureBoxRoad.Top += speed;
+            pictureBoxRoad2.Top += speed;
+
+            // Движение вражеских машинок
+            int CarSpeed = 7;
+            EnemyCar1.Top += CarSpeed;
+            EnemyCar2.Top += CarSpeed;
+ 
+            // движение монетки со скоростью движения дороги
+            Coin.Top += speed;
+
+            if (pictureBoxRoad.Top >= 650)
+            {
+                // Возвращение двух PictureBox на начальные координаты
+                pictureBoxRoad.Top = 0;
+                pictureBoxRoad2.Top = -650;
+            }
+
+            // Спавн вражеских машинок на рандомных координатах
+            if (EnemyCar1.Top >= 650)
+            {
+                EnemyCar1.Top = -130;
+                Random rnd = new Random();
+                EnemyCar1.Left = rnd.Next(150, 290);
+            }
+            if (EnemyCar2.Top >= 650)
+            {
+                EnemyCar2.Top = -400;
+                Random rnd = new Random();
+                EnemyCar2.Left = rnd.Next(350,560);
+            }
+
+            // Проверка столкнвоения машинок
+            if (Car.Bounds.IntersectsWith(EnemyCar1.Bounds) || Car.Bounds.IntersectsWith(EnemyCar2.Bounds)) 
+            {
+                timer.Enabled = false;
+                labelLose.Visible = true;
+                buttonRestart.Visible = true;
+
+                lose = true;
+            }
+
+            if (Coin.Top >= 650)
+            {
+                // возвращение монетки на координаты -50
+                Coin.Top = -50;                
+
+                Random rnd = new Random();
+                Coin.Left = rnd.Next(150, 560);
+            }
+
+            if (Car.Bounds.IntersectsWith(Coin.Bounds)) // проверка координат машинки и монетки
+            {
+                countCoints++;
+                labelCoins.Text = $"Монеты: {countCoints.ToString()}";
+
+                // при сокосновении с монеткой возвращаем ее на рандомные по Х координаты
+                Coin.Top = -50;
+                Random rnd = new Random();
+                Coin.Left = rnd.Next(150, 560);
+            }
+
+                
+        }
+
+        private void FormRace_KeyDown(object sender, KeyEventArgs e)
+        {
+            // движение маишнки после проигрыша
+            if (lose)
+            {
+                return;
+            }
+            
+            int speedCar = 10;
+            // передвижение машинки
+            if ((e.KeyCode == Keys.Left || e.KeyCode == Keys.A) && Car.Left > 150 ) // если больше 150 пикселей, то не выходим за границы
+            {
+                Car.Left -= speedCar;
+            }
+            else if ((e.KeyCode == Keys.Right || e.KeyCode == Keys.D) && Car.Right < 700)
+            {
+                Car.Left += speedCar;
+            }
+            else if ((e.KeyCode == Keys.Up || e.KeyCode == Keys.W) && Car.Top > 5)
+            {
+                Car.Top -= speedCar;
+            }
+            else if ((e.KeyCode == Keys.Down || e.KeyCode == Keys.S) && Car.Top < 520)
+            {
+                Car.Top += speedCar;
+            }
+        }
+
+        private void labelLose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e)
+        {
+            EnemyCar1.Top = -130;
+            EnemyCar2.Top = -400;
+            labelLose.Visible = false;
+            buttonRestart.Visible = false;
+            timer.Enabled = true; // запуск таймера
+            lose = false;
+
+            countCoints = 0;
+            labelCoins.Text = "Монеты: 0";
+            Coin.Top = -500;
+        }
+    }
+}
